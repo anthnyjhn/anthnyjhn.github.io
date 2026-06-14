340482b98d1771e57615ec1895e884a6a9128a20
anthnyjhn <johncaronongan17@gmail.com>
2026-06-14 15:38:23 +1000

    feat: refactor Experience and Projects components to use section tags and improve layout with hover effects

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,62 +5,72 @@ import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 const Experience = () => {
   return (
-    <div className="my-15">
-      <h2 className="my-6 font-semibold">EXPERIENCE</h2>
-      <div className="grid grid-cols-12 mb-12">
-        <div className="col-span-3">
-          <p className="text-sm font-semibold text-zinc-500">2025 - PRESENT</p>
-        </div>
-        <div className="col-span-9 mb-10">
-          <p className="font-semibold text-lg text-zinc-200">
-            IT Specialist (Self-Employed)
-          </p>
-          <p className="font-normal mb-2  text-zinc-300">
-            Condobolin IT Solutions
-          </p>
-          <p className="mb-3 text-zinc-400">
-            Troubleshoot complex infrastracture, provide technical assistance
-            and administer system in production environments. Configure and
-            maintain Linux and Windows server systems and provide updates and
-            recommendations.
-          </p>
-          <div className="flex flex-wrap gap-1.5">
-            <span className="bg-[#141414] px-3 py-1 text-sm rounded-2xl ">
-              Linux
-            </span>
-            <span className="bg-[#141414]  px-3 py-1 text-sm rounded-2xl ">
-              Servers
-            </span>
-            <span className="bg-[#141414]  px-3 py-1 text-sm rounded-2xl ">
-              Customer Support
-            </span>
+    <section className="my-15">
+      <h2 className="my-6 font-semibold" id="EXPERIENCE">
+        EXPERIENCE
+      </h2>
+      <div>
+        <div className="transition-transform duration-300 hover:scale-101 hover:bg-[#0f0f0f] rounded-md p-4 grid grid-cols-12 mb-10">
+          <div className="col-span-3">
+            <p className="text-sm font-semibold text-zinc-500">
+              2025 - PRESENT
+            </p>
+          </div>
+          <div className="col-span-9">
+            <p className="font-semibold text-lg text-zinc-200">
+              IT Specialist (Self-Employed)
+            </p>
+            <p className="font-normal mb-2  text-zinc-300">
+              Condobolin IT Solutions
+            </p>
+            <p className="mb-3 text-zinc-400">
+              Troubleshoot complex infrastracture, provide technical assistance
+              and administer system in production environments. Configure and
+              maintain Linux and Windows server systems and provide updates and
+              recommendations.
+            </p>
+            <div className="flex flex-wrap gap-1.5">
+              <span className="bg-[#141414] px-3 py-1 text-sm rounded-2xl ">
+                Linux
+              </span>
+              <span className="bg-[#141414]  px-3 py-1 text-sm rounded-2xl ">
+                Servers
+              </span>
+              <span className="bg-[#141414]  px-3 py-1 text-sm rounded-2xl ">
+                Customer Support
+              </span>
+            </div>
           </div>
         </div>
-        <div className="col-span-3">
-          <p className="text-sm font-semibold text-zinc-500">2024 - PRESENT</p>
-        </div>
-        <div className="col-span-9">
-          <p className="font-semibold text-lg text-zinc-200">
-            Wide Format Printer Operator
-          </p>
-          <p className="font-normal mb-2  text-zinc-300">
-            Progress Printing Pty Ltd
-          </p>
-          <p className="mb-3 text-zinc-400">
-            Operate, maintain and troubleshoot printing equipments. Work
-            collaboratively with sales team, managers, and print finishers to
-            offer the best output to our clients.
-          </p>
-          <div className="flex flex-wrap gap-1.5">
-            <span className="bg-[#141414] px-3 py-1 text-sm rounded-2xl ">
-              Teamwork
-            </span>
-            <span className="bg-[#141414]  px-3 py-1 text-sm rounded-2xl ">
-              Communication
-            </span>
-            <span className="bg-[#141414]  px-3 py-1 text-sm rounded-2xl ">
-              Creativity
-            </span>
+        <div className="transition-transform duration-300 hover:scale-101 hover:bg-[#0f0f0f] rounded-md p-4 grid grid-cols-12 mb-10">
+          <div className="col-span-3">
+            <p className="text-sm font-semibold text-zinc-500">
+              2024 - PRESENT
+            </p>
+          </div>
+          <div className="col-span-9">
+            <p className="font-semibold text-lg text-zinc-200">
+              Wide Format Printer Operator
+            </p>
+            <p className="font-normal mb-2  text-zinc-300">
+              Progress Printing Pty Ltd
+            </p>
+            <p className="mb-3 text-zinc-400">
+              Operate, maintain and troubleshoot printing equipments. Work
+              collaboratively with sales team, managers, and print finishers to
+              offer the best output to our clients.
+            </p>
+            <div className="flex flex-wrap gap-1.5">
+              <span className="bg-[#141414] px-3 py-1 text-sm rounded-2xl ">
+                Teamwork
+              </span>
+              <span className="bg-[#141414]  px-3 py-1 text-sm rounded-2xl ">
+                Communication
+              </span>
+              <span className="bg-[#141414]  px-3 py-1 text-sm rounded-2xl ">
+                Creativity
+              </span>
+            </div>
           </div>
         </div>
       </div>
@@ -70,7 +80,7 @@ const Experience = () => {
           <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="xs" />
         </a>
       </h2>
-    </div>
+    </section>
   );
 };
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,36 +5,40 @@ import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 const Projects = () => {
   return (
-    <div className="my-15">
-      <h2 className="my-6 font-semibold">PROJECTS</h2>
-      <div className="flex flex-wrap sm:grid grid-cols-12 mb-12">
-        <div className="mb-4 sm:col-span-3 pr-8 pt-1">
-          <img
-            src="/antibrainrot.png"
-            alt=""
-            className="max-w-full rounded hover:outline-1 "
-          />
-        </div>
-        <div className="col-span-9">
-          <p className="font-semibold mb-2 text-lg">
-            Anti-Brainrot Chrome Extension{" "}
-            <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="xs" />
-          </p>
-          <p className="mb-3 text-zinc-400">
-            A browser extension that helps you focus by blocking short form
-            contents and algorithm-suggested contents. Accumulated 520+ installs
-            and 50+ weekly active users.
-          </p>
-          <div className="flex flex-wrap gap-1.5">
-            <span className="bg-[#141414] px-3 py-1 text-sm rounded-2xl ">
-              JavaScript
-            </span>
-            <span className="bg-[#141414]  px-3 py-1 text-sm rounded-2xl ">
-              HTML
-            </span>
-            <span className="bg-[#141414]  px-3 py-1 text-sm rounded-2xl ">
-              Web Scraping
-            </span>
+    <section className="my-15">
+      <h2 className="my-6 font-semibold" id="PROJECTS">
+        PROJECTS
+      </h2>
+      <div>
+        <div className="flex flex-wrap sm:grid grid-cols-12 mb-12">
+          <div className="mb-4 sm:col-span-3 pr-8 pt-1">
+            <img
+              src="/antibrainrot.png"
+              alt=""
+              className="max-w-full rounded hover:outline-1 "
+            />
+          </div>
+          <div className="col-span-9">
+            <p className="font-semibold mb-2 text-lg">
+              Anti-Brainrot Chrome Extension{" "}
+              <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="xs" />
+            </p>
+            <p className="mb-3 text-zinc-400">
+              A browser extension that helps you focus by blocking short form
+              contents and algorithm-suggested contents. Accumulated 520+
+              installs and 50+ weekly active users.
+            </p>
+            <div className="flex flex-wrap gap-1.5">
+              <span className="bg-[#141414] px-3 py-1 text-sm rounded-2xl ">
+                JavaScript
+              </span>
+              <span className="bg-[#141414]  px-3 py-1 text-sm rounded-2xl ">
+                HTML
+              </span>
+              <span className="bg-[#141414]  px-3 py-1 text-sm rounded-2xl ">
+                Web Scraping
+              </span>
+            </div>
           </div>
         </div>
       </div>
@@ -44,7 +48,7 @@ const Projects = () => {
           <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="xs" />
         </a>
       </h2>
-    </div>
+    </section>
   );
 };
 
